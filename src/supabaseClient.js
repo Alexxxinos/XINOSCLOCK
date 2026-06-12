c07cea2d27e28f74a94f1dd8a048be779a7e1ca2
Alexxxinos <asakarellos@xinoscorp.com>
2026-06-12 15:29:51 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gswglydpojjhxczcfzps.supabase.co";
-const supabaseKey = "sb_publishable_FW4ueA4KVWAZYKPXqIVWHw_mUS-HJww";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdzd2dseWRwb2pqaHhjemNmenBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNzk3MTksImV4cCI6MjA5Njg1NTcxOX0.xa0vtDBpsfxBtOBsUKnL2vrvj7MNbK7IWUCxkxD2OAk";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
